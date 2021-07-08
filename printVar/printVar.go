5c90b24066ecdafe3f79f2839bcc0967e95f0344
goelvatsal <goel.vatsal@outlook.com>
2021-07-08 13:59:00 -0400

    cleaning repository

--- printVar/printVar.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var speed int
-
-	fmt.Println(speed)
-}
--- splitURL/modifiedSplit.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import ("fmt"; 	"path")
-
-func main() {
-	dir, _ := path.Split("css/main.css")
-
-	fmt.Println("dir :", dir)
-}
