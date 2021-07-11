e9987fbd0408e29478f5213e840ea265504a405a
goelvatsal <goel.vatsal@outlook.com>
2021-07-11 14:57:55 -0400

    adding more exercises

---
+++ section1/exercises02/exercises/01-count-arguments/main.go
@@ -0,0 +1,28 @@
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Count the Arguments
+//
+//  Print the count of the command-line arguments
+//
+// INPUT
+//  bilbo balbo bungo
+//// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+// EXPECTED OUTPUT
+//  There are 3 names.
+// ---------------------------------------------------------
+
+func main() {
+	// UNCOMMENT & FIX THIS CODE
+	// count := ?
+
+	// UNCOMMENT IT & THEN DO NOT TOUCH THIS CODE
+	// fmt.Printf("There are %d names.\n", count)
+}
