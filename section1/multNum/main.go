ed17883862db51c5713a7372996115bc04f6c36b
goelvatsal <goel.vatsal@outlook.com>
2021-07-26 10:11:00 -0400

    working on program

--- section1/multNum/main.go
+++ section1/multNum/main.go
@@ -7,8 +7,7 @@ import (
 )
 
 func main() {
-	a := os.Args
-	if len(a) != 2 {
+	if a := os.Args; len(a) != 2 {
 		fmt.Printf("Give me a number.\n")
 	} else if n, err := strconv.Atoi(a[1]); err != nil {
 		fmt.Printf("Cannot convert %q\n", a[1])
