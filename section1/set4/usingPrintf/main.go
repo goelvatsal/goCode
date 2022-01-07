e5ca7677b70555c0c3629282a825a2fb8d7ae044
goelvatsal <goel.vatsal@outlook.com>
2022-01-07 13:32:00 -0500

    completed example programs

---
+++ section1/set4/usingPrintf/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		planet   = "Venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+
+	fmt.Printf("Planet: %v\n", planet)
+	fmt.Printf("Distance: %v million kms\n", distance)
+	fmt.Printf("Orbital Period: %v days\n", orbital)
+	fmt.Printf("Does %v have life? %v\n", planet, hasLife)
+
+	fmt.Printf("%v is %v million kms away from Earth. "+
+		"Think! %[2]v million kms! %[1]v OMG!\n", planet, distance)
+
+}
