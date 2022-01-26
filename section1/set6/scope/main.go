7f409b859aaca9cf64d6b48f3f9696ccdd2382ca
goelvatsal <goel.vatsal@outlook.com>
2022-01-25 21:44:33 -0500

    completed example exercises

---
+++ section1/set6/scope/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if a := os.Args; len(a) != 2 {
+		fmt.Println("Give me a number.")
+	} else if n, err := strconv.Atoi(a[1]); err != nil {
+		fmt.Printf("Cannot convert %q.\n", a[1])
+	} else {
+		fmt.Printf("%s * 2 is %d.\n", a[1], n*2)
+	}
+}
