8ace7e54ac8dbfb8fc8315bd41b4510f1af03ed3
Vatsal Goel <goel.vatsal@outlook.com>
2022-02-20 12:53:57 -0500

    fixed problems; added comments

--- section1/set8/nestedLoops/challenge.go
+++ section1/set8/nestedLoops/challenge.go
@@ -1,5 +1,13 @@
 package main
 
+// input: an integer for what number to make the table for
+// if input is 5; then program prints 5x5 table
+
+// output: the table by which the user inputted the number
+// input = 5; output is 5x5 table
+
+// goal: print multiplication table
+
 import (
 	"fmt"
 	"os"
@@ -14,15 +22,18 @@ func main() {
 		return
 	}
 
+	//prints header column
 	fmt.Printf("%5s", "X")
 	for i := 0; i <= n; i++ {
 		fmt.Printf("%5d", i)
 	}
 	fmt.Println()
 
+	// prints vertical column
 	for i := 0; i <= n; i++ {
 		fmt.Printf("%5d", i)
 
+		// multiplication code
 		for j := 0; j <= n; j++ {
 			fmt.Printf("%5d", j*i)
 		}
