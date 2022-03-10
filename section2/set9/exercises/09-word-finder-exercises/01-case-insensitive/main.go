f676fc0fcdee7ec2f4018ac32da1c841318ab0c8
goelvatsal <goel.vatsal@outlook.com>
2022-03-09 22:36:24 -0500

    completed example exercises and completed challenge moodly

---
+++ section2/set9/exercises/09-word-finder-exercises/01-case-insensitive/main.go
@@ -0,0 +1,46 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Case Insensitive Search
+//
+//  Allow for case-insensitive searching
+//
+// EXAMPLE
+//  Let's say that the user runs the program like this:
+//    go run main.go LAZY
+//
+//  Or like this: go run main.go lAzY
+//  Or like this: go run main.go lazy
+//
+//  For all cases above, the program should find
+//  the "lazy" keyword.
+// ---------------------------------------------------------
+
+func main() {
+	const corpus = "lazy cat jumps again and again and again" + ""
+	words := strings.Fields(corpus)
+	query := os.Args[1:]
+
+	for _, q := range query {
+		for i, w := range words {
+			if strings.ToLower(q) == strings.ToLower(w) {
+				fmt.Printf("#%-2d: %q\n", i+1, w)
+			}
+		}
+	}
+
+}
---
+++ section3/set10/compLiteral/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	books := [...]string{"Kafka's Revenge", "Stay Golden"}
+
+	fmt.Printf("%q\n", books)
+}
