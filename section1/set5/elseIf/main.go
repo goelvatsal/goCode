af7cb4dcf3e6601d9112f7970b0cc5bbb9acddbe
goelvatsal <goel.vatsal@outlook.com>
2022-08-09 12:46:37 -0400

    completed exercises and examples

---
+++ section1/set5/elseIf/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	score := 5
+
+	if score > 3 {
+		fmt.Println("Good!")
+	} else if score == 3 {
+		fmt.Println("On the edge")
+	} else if score == 2 {
+		fmt.Println("Meh...")
+	} else {
+		fmt.Println("Low")
+	}
+}
